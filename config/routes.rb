3e6d2cf8be1eb8df4825139f51b9a9aba9d129ec
Leo <ronald.mauritsson@gmail.com>
2019-12-04 14:25:01 +0300

    changed links on header to display differently depending on login status

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   get 'users/create'
   get 'users/show'
+  
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  
+
   resources :users
 end
