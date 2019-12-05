ab8ec11f8968620257e9b6a013bfe13964891d4a
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-12-05 13:09:50 -0300

    added attending?, attend! and cancel! methods to user_controller, added also the html section of this upcoming methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,23 @@ class UsersController < ApplicationController
   end
 
   def upcoming_events
-    self.attended_events.upcoming
+    self.attended_events.upcoming(current_user.id, event.id)
   end
 
   def previous_events
     self.attended_events.past
+  end
+
+  def attending?(event)
+  event.attendees.include?(self)
+  end
+
+  def attend!(event)
+    self.event_attendees.create!(attended_event_id: event.id)
+  end
+
+  def cancel!(event)
+    self.event_attendees.find_by(attended_event_id: event.id).destroy
   end
 
   private
