c355e21a6959ce959d2fff9619af01fe39df376e
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-12-09 19:12:03 -0300

    added else statement to the display button, added a restict_access to the login_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :restrict_access
+
   def new
     @user = User.new
   end
@@ -25,4 +27,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def restrict_access
+	  redirect_to login_path || signup_path unless current_user
+	end
 end
